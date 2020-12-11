b98099c0fe17b49b70a66cfd48754e53be1ac881
SwapnilMahmud <mahmudaiub51@gmail.com>
2020-12-11 19:33:57 +0600

    v4.1
    v4.1

--- admin/delete-post.php
+++ admin/delete-post.php
@@ -3,6 +3,12 @@ include"config.php";
 $post_id=$_GET['id'];
 $cat_id=$_GET['catid'];
 
+$sql11="SELECT * FROM post WHERE post_id ={$post_id}";
+$result=mysqli_query($conn,$sql11) or die("query failed..");
+$row=mysqli_fetch_assoc($result);
+unlink("upload/".$row['post_img']);
+
+
 $sql="DELETE FROM post WHERE post_id={$post_id};";
 $sql .="UPDATE category SET post=post -1 WHERE category_id={$cat_id}";
 
